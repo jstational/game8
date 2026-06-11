fe62a8b8c6048371b75a41857babfa655f90fb70
jstation <sophiabeachanel@gmail.com>
2026-06-11 18:34:16 +0800

    Update g8b.java

--- src/g8/world/blocks/g8b.java
+++ src/g8/world/blocks/g8b.java
@@ -1,10 +1,12 @@
+package game8.blocks
+
 import mindustry.world.Blocks;
 import arc.math.geom.Vec2;
 
 public class g8b extends Blocks {
-  public Vec2 press = new Vec2();
-  public boolean pressreq = new boolean();
-  public float pressreqfluct = new float();
-  public boolean envpress = new boolean();
-  public int envpressmult = new int();
+  private Vec2 press;
+  private boolean pressreq;
+  private float pressreqfluct;
+  private boolean envpress;
+  private int envpressmult;
 }
