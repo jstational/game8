e2992e9f8a956e920bc15fae324fb7c4202cb18e
jstation <sophiabeachanel@gmail.com>
2026-06-13 17:00:44 +0800

    Update and rename envpressgen.java to PressureGenerator.java

--- src/g8/world/blocks/production/PressureGenerator.java
+++ src/g8/world/blocks/production/PressureGenerator.java
@@ -1,11 +1,11 @@
 package game8.blocks.production
 
 import mindustry.world.blocks.environment.Floor;
-import mindustry.world.blocks.enviironment.StaticWall
+import mindustry.world.blocks.environment.StaticWall;
 import arc.struct.Seq;
 import game8.blocks;
 
-public class envpressgen extends g8b {
+public class PressureGenerator extends g8b {
   private Seq<Floor> tileboost = new Seq<>();
   private Seq<StaticWall> wallboost = new Seq<>();
 }
